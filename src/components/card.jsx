5d3d70d3141cc07e1f26102dd87e36e4c15bfc38
Maha Abo Assi <maha@onekw.com>
2025-05-18 16:43:52 +0400

    ensure fo every updates

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Card = ({title, type,content=[], subTitle, description, img, link=""})=>{
     return(<div className="card p-2" >
-                <div className="p-4 h-full blur-bg content-card  radius-border p-4 sm:p-6  md:p-10 lg:p-16 flex flex-col gap-4">
+                <div className="p-4  blur-bg content-card  radius-border h-full sm:p-6  md:p-10 lg:p-16 flex flex-col gap-4">
                     <div className="flex items-center gap-5">
                         <div>{img}</div>
                         <div>
